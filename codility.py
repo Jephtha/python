8de4e4428bc6cce781af88f13991fdc724a4f56f
Jeff Anga <israeljay012@gmail.com>
2022-09-09 20:22:00 -0230

    coding exercise

--- codility.py
+++ codility.py
@@ -1,7 +1,26 @@
+# My solutions to the Lessons on https://app.codility.com/programmers/lessons/1-iterations/
+# Started saving my solutions late so I don't have the first few code
 
-def minpositive(a):
-    A = set(a)
-    ans = 1
-    while ans in A:
-       ans += 1
-    return ans
+# Lesson 2, Exercise 2: OddOccurrencesInArray first solution (using list methods)
+def solution(A):
+    # write your code in Python 3.6
+    for x in A:
+        if A.count(x) % 2 == 1:
+            return x
+
+# time complexity of O(n^2), got a score of 66%, with a 100% correctness. the problem
+# was the the time complexity. code is inefficient on larger arrays. so i came up with another solution
+
+# Lesson 2, Exercise 2: OddOccurrencesInArray 2nd solution (using dictionaries)
+def solution(A):
+    # write your code in Python 3.6
+    B = {}
+    for x in A:
+        if x in B:
+          B[x] += 1
+        else:
+            B[x] = 1
+
+    for x in B:
+        if B[x] % 2 == 1:
+            return x  
--- plotly.py
+++
@@ -1,5 +0,0 @@
-import wget
-
-#url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0321EN-SkillsNetwork/datasets/spacex_launch_dash.csv"
-
-wget.download("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DS0321EN-SkillsNetwork/labs/module_3/spacex_dash_app.py")
