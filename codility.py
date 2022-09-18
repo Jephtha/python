1abfd91fcdf9a10f923479a76ce4c32e51691032
Jeff Anga <israeljay012@gmail.com>
2022-09-18 19:12:19 -0230

    updated codility.py

--- codility.py
+++ codility.py
@@ -191,3 +191,72 @@ def solution(A):
 
 print(solution(A))
 
+
+
+
+
+
+
+
+
+# Bug Fixing Leader Sorted
+# A non-empty array A consisting of N integers and sorted in a non-decreasing order (i.e. A[0] ≤ A[1] ≤ ... ≤ A[N−1]) is given. The leader of this array is the value that occurs in more than half of the elements of A.
+
+# You are given an implementation of a function:
+
+# def solution(A)
+
+# that, given a non-empty array A consisting of N integers, sorted in a non-decreasing order, returns the leader of array A. The function should return −1 if array A does not contain a leader.
+
+# For example, given array A consisting of ten elements such that:
+
+#   A[0] = 2
+#   A[1] = 2
+#   A[2] = 2
+#   A[3] = 2
+#   A[4] = 2
+#   A[5] = 3
+#   A[6] = 4
+#   A[7] = 4
+#   A[8] = 4
+#   A[9] = 6
+# the function should return −1, because the value that occurs most frequently in the array, 2, occurs five times, and 5 is not more than half of 10.
+
+# Given array A consisting of five elements such that:
+
+#   A[0] = 1
+#   A[1] = 1
+#   A[2] = 1
+#   A[3] = 1
+#   A[4] = 50
+# the function should return 1.
+
+# The attached code is still incorrect for some inputs. Despite the error(s), the code may produce a correct answer for the example test cases. The goal of the exercise is to find and fix the bug(s) in the implementation. You can modify at most three lines.
+
+# Assume that:
+
+# N is an integer within the range [1..100,000];
+# each element of array A is an integer within the range [0..2,147,483,647];
+# array A is sorted in non-decreasing order.
+# In your solution, focus on correctness. The performance of your solution will not be the focus of the assessment.
+
+
+A = [2, 2, 2, 2, 2, 3, 4, 4, 4, 6]
+B = [1, 1, 1, 1, 50]
+
+def solution(A):
+    n = len(A)
+    count = 0
+    candidate = A[n//2]
+
+    for i in range(n):
+        if (A[i] == candidate):
+            count += 1
+
+    if (count*2 > n):
+        return candidate
+
+    return -1
+
+print(solution(A))
+print(solution(B))
